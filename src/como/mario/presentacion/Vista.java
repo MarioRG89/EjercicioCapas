248d1cda5a2acd76177617cdf6c85b499eaeb581
mario <mariorebollogarcia89@gmail.com>
2020-10-01 09:29:27 +0200

    [ADD] fichero gitignore y clase vista

---
+++ src/como/mario/presentacion/Vista.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package como.mario.presentacion;
+
+/**
+ * @author mario
+ *
+ */
+public class Vista {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Apéndice de método generado automáticamente
+
+	}
+
+}
