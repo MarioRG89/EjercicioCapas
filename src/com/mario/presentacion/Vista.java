bf7384621f0976dce77b097d65f25e9ce0e20c3e
mario <mariorebollogarcia89@gmail.com>
2020-10-01 12:11:21 +0200

    [MOD] modificacion de nombre de paquete

--- src/com/mario/presentacion/Vista.java
+++ src/com/mario/presentacion/Vista.java
@@ -1,24 +1,12 @@
-/**
- * 
- */
-package como.mario.presentacion;
+package com.mario.presentacion;
+
 
 import java.io.IOException;
 import java.util.Scanner;
-
-
 import com.mario.negocio.LogicaNegocio;
 
 
-/**
- * @author mario
- *
- */
 public class Vista {
-
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
 		Scanner sc= new Scanner(System.in);
 		System.out.println("introduce el nombre de la persona a la que aplicarle el descuento" + "\n");
@@ -29,8 +17,10 @@ public class Vista {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		sc.close();
+			sc.close();
 	}
 
 }
 
+
+	
